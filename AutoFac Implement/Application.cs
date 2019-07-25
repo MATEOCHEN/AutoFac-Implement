835e1cbbe0b9db70c2dab2ac73fc4135cf54ab1a
Steven Chen <steven.chen19@TI03NB69>
2019-07-25 19:53:30 +0800

    finish

--- AutoFac Implement/Application.cs
+++ AutoFac Implement/Application.cs
@@ -4,6 +4,31 @@ using Autofac;
 
 namespace AutoFac_Implement
 {
+    internal interface IAccountRepo
+    {
+        List<string> GetAccount();
+    }
+
+    internal interface ILogger
+    {
+        void RecordLog(string message);
+    }
+
+    internal class AdminRepo : IAccountRepo
+    {
+        public List<string> GetAccount()
+        {
+            return new List<string>()
+            {
+                "maruko",
+                "guy",
+                "kyo",
+                "angel",
+                "april"
+            };
+        }
+    }
+
     internal class Application
     {
         private static void Main(string[] args)
@@ -11,11 +36,27 @@ namespace AutoFac_Implement
             var builder = new ContainerBuilder();
             builder.RegisterType<AdminRepo>().As<IAccountRepo>();
             builder.RegisterType<ConsoleLogger>().As<ILogger>();
+            builder.RegisterType<LoginService>();
             var container = builder.Build();
+            var loginService = container.Resolve<LoginService>();
+            loginService.Login("maruko", "123456");
+            Console.Read();
+        }
+    }
 
-            //var loginService = new LoginService(new AdminRepo(), new ConsoleLogger());
-            //loginService.Login("steven", "123456");
-            //Console.Read();
+    internal class ConsoleLogger : ILogger
+    {
+        public void RecordLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    internal class Logger : ILogger
+    {
+        public void RecordLog(string message)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -30,62 +71,28 @@ namespace AutoFac_Implement
             _logger = logger;
         }
 
-        public void Login(string acc, string pwd)
-        {
-            var message = "";
-            message = Auth(acc, pwd) ? $"{acc} is login success!!" : $"{acc} is not the right account!!";
-            _logger.RecordLog(message);
-        }
-
         public bool Auth(string account, string password)
         {
             var result = false;
-            //var accountList = _accountRepo.GetAccount();
-            //foreach (var adminAccount in accountList)
-            //{
-            //    if (account != adminAccount) continue;
-            //    result = true;
-            //    break;
-            //}
+            var accountList = _accountRepo.GetAccount();
+            foreach (var adminAccount in accountList)
+            {
+                if (account != adminAccount) continue;
+                result = true;
+                break;
+            }
             if (account == "steven" && password == "123456")
             {
                 result = true;
             }
             return result;
         }
-    }
 
-    internal interface ILogger
-    {
-        void RecordLog(string message);
-    }
-
-    internal class Logger : ILogger
-    {
-        public void RecordLog(string message)
+        public void Login(string acc, string pwd)
         {
-            throw new NotImplementedException();
-        }
-    }
-
-    internal class ConsoleLogger : ILogger
-    {
-        public void RecordLog(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-
-    internal interface IAccountRepo
-    {
-        List<string> GetAccount();
-    }
-
-    internal class AdminRepo : IAccountRepo
-    {
-        public List<string> GetAccount()
-        {
-            throw new NotImplementedException();
+            var message = "";
+            message = Auth(acc, pwd) ? $"{acc} is login success!!" : $"{acc} is not the right account!!";
+            _logger.RecordLog(message);
         }
     }
 }
